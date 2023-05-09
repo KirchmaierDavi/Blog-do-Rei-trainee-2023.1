5bde5099d66fc1e1213fabe5ce5f57f139d9d08a
Gabriel Souza <gabrielsaojoao12@gmail.com>
2023-05-09 20:55:06 -0300

    Implementado pedidos do cliente e alterado css para corresponder a lista de usuários

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,17 +1,42 @@
-var modal = document.getElementById("modal");
-var span = document.getElementsByClassName("close")[0];
-var btn = document.getElementById("new-post");
+let modal = document.getElementById("dashboard-functions");
+let spans = document.querySelectorAll('[id^="modal-"]');
+let btn = document.getElementById("new-post");
 
-btn.onclick = function(){
-    modal.style.display = "block";
-}
 
-span.onclick = function(){
+
+
+spans.forEach(element => {
+
+  let closeButton = element.querySelector('.modal-close');
+
+  closeButton.addEventListener('click', () => {
+
     modal.style.display = "none";
+    closeModals();
+  });
+});
+
+
+function dashboardFunctions(button){
+    let showModal = document.getElementById("modal-"+button.id);
+    console.log(button)
+    modal.style.display = "block";
+    showModal.style.display = "block";
 }
 
 window.onclick = function(event){
     if(event.target == modal){
         modal.style.display = "none";
+        closeModals();
     }
+
+
+}
+
+function closeModals(){
+    let modalElements = document.querySelectorAll('[id^="modal-"]');
+
+    modalElements.forEach(element => {
+        element.style.display = "none";
+      });
 }
