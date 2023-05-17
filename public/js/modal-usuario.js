e6d24bf081e1b4be5ca97e81e763b5c9485b105a
SaraIngrid <saraingrid2505@gmail.com>
2023-05-17 14:14:14 -0300

    Alterações modal

--- public/js/modal-usuario.js
+++ public/js/modal-usuario.js
@@ -1,20 +1,13 @@
-
-const editarBtn = document.getElementById('editarBtn');
-
+const editarBtn = document.querySelector('.btn-warning');
 
 const modalEditUser = document.getElementById('modal-edit-user');
 
-
 editarBtn.addEventListener('click', function() {
- 
   modalEditUser.style.display = 'block';
 });
 
-
 const modalClose = document.querySelector('.modal-close');
 
-
 modalClose.addEventListener('click', function() {
-
   modalEditUser.style.display = 'none';
 });
