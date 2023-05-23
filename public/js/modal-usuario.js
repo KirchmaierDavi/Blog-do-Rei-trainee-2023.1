452160116c2c0d344ba465c22358074278b341df
SaraIngrid <saraingrid2505@gmail.com>
2023-05-23 13:08:52 -0300

    Edição de usuário

--- public/js/modal-usuario.js
+++ public/js/modal-usuario.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("edit-user");
-    const cancelar = document.getElementById("cancelar");
-    const confirmar = document.getElementById("confirmar");
-    const excluir = document.getElementById("EditarUsuario");
-  
-    excluir.addEventListener("click", function() {
+  const modal = document.getElementById("edit-user");
+  const abrirModal = document.getElementById("abrir-modal");
+  const cancelar = document.getElementById("cancelar");
+  const confirmar = document.getElementById("confirmar");
+
+  abrirModal.addEventListener("click", function() {
       modal.style.display = "block";
-    });
-  
-    cancelar.addEventListener("click", function() {
+  });
+
+  cancelar.addEventListener("click", function() {
       modal.style.display = "none";
-    });
-  
-    confirmar.addEventListener("click", function() {
+  });
+
+  confirmar.addEventListener("click", function() {
       modal.style.display = "none";
-    });
-  }); 
+  });
+});
