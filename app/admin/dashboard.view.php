0767dbbb23b70ca77a415d434db09aaed05f6d7b
amrlrayssa <amrlrayssa@gmail.com>
2023-06-19 22:04:16 -0300

    arrumando as fontes

--- app/admin/dashboard.view.php
+++ app/admin/dashboard.view.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="../../public/css/dashboard.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
