f3fc8707bbc340dd46c2596883b43dc93c647016
ssurerus <saulosurerus@hotmail.com>
2023-06-21 13:06:50 -0300

    refatoracao da funcao edit dashboard content

--- app/Controllers/CustomContentController.php
+++ app/Controllers/CustomContentController.php
@@ -19,42 +19,32 @@ class CustomContentController
 
     public function edit()
     {
+        $parameters = [
+            'about_us' => $_POST['about_us'],
+            'memorium' => $_POST['memorium'],
+            'main_color' => $_POST['main-color'],
+            'main_color_hover' => $_POST['main-color-hover'],
+            'secondary_color' => $_POST['second-color'],
+            'secondary_color_hover' => $_POST['second-color-hover'],
+            'headers_colors' => $_POST['headers-colors'],
+            'about_us_links_colors' => $_POST['about-us-link-color'],
+        ];
         var_dump($_FILES);
-        if($_FILES['logo_image']['name'] === ''){
-            $parameters = [
-                'about_us' => $_POST['about_us'],
-                // 'image' =>  $pasta . $novoNome . "." . $extencao,
-                'memorium' => $_POST['memorium'],
-                'main_color' => $_POST['main-color'],
-                'main_color_hover' => $_POST['main-color-hover'],
-                'secondary_color' => $_POST['second-color'],
-                'secondary_color_hover' => $_POST['second-color-hover'],
-                'headers_colors' => $_POST['headers-colors'],
-                'about_us_links_colors' => $_POST['about-us-link-color'],
-            ];
-            App::get('database')->edit('blog_content',1,$parameters);
-            header('location: /admin/customize');
-        }
-        else{
+        if($_FILES['logo_image']['name'] !== ''){
             $arquivo = $_FILES['logo_image']['name'];
             $novoNome = uniqid();
             $pasta = 'public/img/';
             $extencao = strtolower(pathinfo($arquivo, PATHINFO_EXTENSION));
             move_uploaded_file($_FILES['logo_image']['tmp_name'], $pasta . $novoNome . "." . $extencao);
-    
-            $parameters = [
-                'logo_image' =>  $pasta . $novoNome . "." . $extencao,
-                'about_us' => $_POST['about_us'],
-                'memorium' => $_POST['memorium'],
-                'main_color' => $_POST['main-color'],
-                'main_color_hover' => $_POST['main-color-hover'],
-                'secondary_color' => $_POST['secondary-color'],
-                'secondary_color_hover' => $_POST['secondary-color-hover'],
-                'headers_colors' => $_POST['headers-colors'],
-                'about_us_link_colors' => $_POST['about-us-link-color'],
-            ];
+            $parameters += ['logo_image' =>  $pasta . $novoNome . "." . $extencao,];
+        }
             App::get('database')->edit('blog_content',1,$parameters);
             header('location: /admin/customize');
-        }
+        
+    }
+
+    public function reset()
+    {
+
     }
 }
